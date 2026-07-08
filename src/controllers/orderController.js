a119b24cd634793681c351cd20b940d7b8465dc7
ajpandit24 <ajpandit24@gmail.com>
2026-07-08 17:38:53 +0530

    Spell mistakes corrected

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -56,7 +56,7 @@ exports.createOrder = async (req, res) => {
 
         console.log("Live Step 1: Initiating email call...");
 
-        await emailService.sendOrderEmail(invoicePayload);
+       const emailResult = await emailService.sendOrderEmail(invoicePayload);
 
         console.log("Live Step 2: Email service results:", emailResult);
         // Generate WhatsApp link
